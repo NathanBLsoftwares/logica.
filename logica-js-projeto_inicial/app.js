c09e60c9a3fb209d58dd52dd09affb2087ab148c
NathanBLsoftwares <nathan@blsoftwares.com.br>
2025-02-21 09:31:37 -0300

    Atividade de 10 pergunrtas - ALURA

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,19 @@
+alert ("Boas vindas ao nosso site!!");
+// let nome = "Lua";
+// let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert ("Preencher todos os campos!!");
+let mensagemDeErro = ("Erro! Preencher todos os campos");
+alert (mensagemDeErro);
+let nome = prompt ("Digite seu nome");
+let idade = prompt ("Digite sua idade");
+
+if (idade >= 18){
+    alert("Você pode tirar a habilitação!!")
+}
+
+else{
+    alert ("Espere mais um pouco")
+}
+
